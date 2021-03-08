46bcc194234daee52b7ff922c23085dfddcca8e7
YHcoLA <nyh7727@gmail.com>
2021-03-08 23:54:39 +0900

    20210308

--- js/ver.js
+++ js/ver.js
@@ -1,19 +1,29 @@
 $(document).ready(function () {
-    setTimeout(function(){
+    var scrollY;
+
+    setTimeout(function () {
         $('#header h1').addClass('fade');
         $('#nav').addClass('down');
     }, 1500);
+
     $(window).on('scroll', function () {
-        var scrollY = $(this).scrollTop() + $(this).height();
-        if(scrollY > $('#container').offset().top){
+        scrollY = $(window).scrollTop() + $(window).height();
+
+        console.log(scrollY);
+        if (scrollY > $('#container').offset().top) {
             $('#nav').removeClass('down');
-        }else{
+        } else {
             $('#nav').addClass('down');
         }
     });
-    $('#nav').on({'mouseenter': function(){
-        $('#nav').addClass('down');
-    }, 'mouseleave' : function(){
-        $('#nav').removeClass('down');
-    }});
+    $('#nav').on({
+        'mouseenter': function () {
+            $('#nav').addClass('down');
+        },
+        'mouseleave': function () {
+            if (scrollY > $('#container').offset().top) {
+                $('#nav').removeClass('down');
+            }
+        }
+    });
 });
