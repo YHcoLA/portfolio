6d998133d52a821359a12aebc21c44f5c28af56f
YHcoLA <nyh7727@gmail.com>
2021-03-09 14:53:23 +0900

    20210309

--- js/ver.js
+++ js/ver.js
@@ -16,6 +16,7 @@ $(document).ready(function () {
             $('#nav').addClass('down');
         }
     });
+    
     $('#nav').on({
         'mouseenter': function () {
             $('#nav').addClass('down');
